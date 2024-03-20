be2547ef58352d3ff3835fd9f51a1ce871d50173
TopGitSan <c.toparcean@gmail.com>
2024-03-20 20:20:37 +0100

    feat(score/frame): :sparkles: create the score and frame components and route to score

---
+++ src/app/frame/frame.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-frame',
+  standalone: true,
+  imports: [],
+  templateUrl: './frame.component.html',
+  styleUrl: './frame.component.scss'
+})
+export class FrameComponent {
+}
---
+++ src/app/score/score.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { FrameComponent } from '../frame/frame.component';
+
+@Component({
+  selector: 'app-score',
+  standalone: true,
+  imports: [FrameComponent],
+  templateUrl: './score.component.html',
+  styleUrl: './score.component.scss'
+})
+export class ScoreComponent {
+
+}
