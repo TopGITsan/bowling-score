ae03483532b4ff3728793220c89fce255c1046ad
TopGitSan <c.toparcean@gmail.com>
2024-03-21 18:10:28 +0100

    feat(helper): :sparkles: create mono type operator that emits on destroy hook

--- src/app/utils/app.helper.ts
+++ src/app/utils/app.helper.ts
@@ -1,3 +1,20 @@
+import { DestroyRef, inject } from '@angular/core';
+import { MonoTypeOperatorFunction, Subject, takeUntil } from 'rxjs';
+
 export function numberToString(value: number | undefined) {
   return value?.toString() ?? '';
 }
+
+export function untilDestroyed(): <T>() => MonoTypeOperatorFunction<T> {
+  const subject = new Subject();
+
+  inject(DestroyRef).onDestroy(() => {
+    subject.next(true);
+    subject.complete();
+  });
+
+  return <T>() => takeUntil<T>(subject.asObservable());
+}
+
+export const random = (min: number, max: number): number =>
+  Math.floor(Math.random() * (max - min)) + min;
