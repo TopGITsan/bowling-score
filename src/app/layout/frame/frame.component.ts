b10bb565a565814775ac95f27396c473195c76f2
TopGitSan <c.toparcean@gmail.com>
2024-03-21 18:20:42 +0100

    feat(score/frame): :sparkles: use onPush detection strategy, and provide state to component

--- src/app/layout/frame/frame.component.ts
+++ src/app/layout/frame/frame.component.ts
@@ -1,18 +1,18 @@
-import { Component, Input } from '@angular/core';
-import { numberToString } from '../../utils/app.helper';
 import { NgClass } from '@angular/common';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
+import { numberToString } from '../../utils/app.helper';
 
 @Component({
   selector: 'app-frame',
   standalone: true,
   imports: [NgClass],
   templateUrl: './frame.component.html',
-  styleUrl: './frame.component.scss'
+  styleUrl: './frame.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class FrameComponent {
   @Input() score: number = 0;
   @Input() isCurrent: boolean = false;
-  @Input({transform: numberToString}) firstAttempt: string = '';
-  @Input({transform: numberToString}) secondAttempt: string = '';
-
+  @Input({ transform: numberToString }) firstAttempt: string = '';
+  @Input({ transform: numberToString }) secondAttempt: string = '';
 }
--- src/app/layout/score/score.component.ts
+++ src/app/layout/score/score.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  ChangeDetectorRef,
+  Component,
+  OnInit,
+  inject,
+} from '@angular/core';
+import { GameStoreService } from '../../store/game-store.service';
+import { untilDestroyed } from '../../utils/app.helper';
 import { FrameComponent } from '../frame/frame.component';
+import { Game } from '../../model/game.model';
 
 @Component({
   selector: 'app-score',
   standalone: true,
   imports: [FrameComponent],
   templateUrl: './score.component.html',
-  styleUrl: './score.component.scss'
+  styleUrl: './score.component.scss',
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ScoreComponent {
+export class ScoreComponent implements OnInit {
+  private readonly gameStoreService: GameStoreService =
+    inject(GameStoreService);
+
+  private readonly changeDetectorRef = inject(ChangeDetectorRef);
+  private readonly untilDestroyed = untilDestroyed();
+
+  state: Game | undefined;
+
+  ngOnInit(): void {
+    this.gameStoreService.state$.pipe(this.untilDestroyed()).subscribe((state)=> {
+      this.state = state;
+      this.changeDetectorRef.detectChanges();
+    });
+  }
 }
